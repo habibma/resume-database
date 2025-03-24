1fa53eb66e2725b93ff542cf76b30cd70ceba16b
habib motavassel <habib.motavassel@gmail.com>
2025-03-24 15:15:13 +0100

    To add pdo.php

---
+++ pdo.php
@@ -0,0 +1,9 @@
+<?php
+try {
+    $pdo = new PDO('mysql:host=localhost;port=3306;dbname=pro','fred', 'zap');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+    die();
+}
+?>
